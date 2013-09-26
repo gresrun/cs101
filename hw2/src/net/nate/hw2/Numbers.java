c459e5d9be988cfdd9437d3cd79ed04b41d0659d
Nathan Hammond <natehamm66@gmail.com>
2013-09-25 20:00:31 -0400

    Homework # 2
    Counting from 5 up to 500 printing only 500 as requested.

---
+++ hw2/src/net/nate/hw2/Numbers.java
@@ -0,0 +1,16 @@
+package net.nate.hw2;
+
+public class Numbers {
+public static void main (String[] args){
+	int x = 5;
+	while(x < 500){
+		x = x+5;
+			}
+	if (x == 500){
+	System.out.println(x);
+	}
+		
+	}
+		}
+
+
