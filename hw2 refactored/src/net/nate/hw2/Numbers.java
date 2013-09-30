5cad4d5d70d4289d2f203efa5f17ee468785bf95
Greg Haines <gresrun@gmail.com>
2013-09-30 09:24:13 -0400

    A working version of Numbers

--- hw2 refactored/src/net/nate/hw2/Numbers.java
+++ hw2 refactored/src/net/nate/hw2/Numbers.java
@@ -2,14 +2,12 @@ package net.nate.hw2;
 
 public class Numbers {
     public static void main(String[] args) {
+        int sum = 0;
         int x = 5;
-        int y = x + 5;
-        int i = 0;
-        for (i = 0; i <= 100; i++) {
-            x = x + y;
-            if (i == 100) {
-                System.out.println(x);
-            }
+        while (x <= 500) {
+            sum += x;
+            x += 5;
         }
+        System.out.println(sum);
     }
 }
